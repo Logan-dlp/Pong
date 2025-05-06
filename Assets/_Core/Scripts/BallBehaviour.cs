06909b7b43a11feac0eabc6971b5a87915a7c06f
MagnaAxel <axel.magna.pro@gmail.com>
2025-05-06 16:13:34 +0200

    reworked to avoid border ignore at high speed

--- Assets/_Core/Scripts/BallBehaviour.cs
+++ Assets/_Core/Scripts/BallBehaviour.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 namespace Ball
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class BallBehaviour : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        [SerializeField] private float _initialSpeed;
+        [SerializeField] private float _maximalSpeed;
+        [SerializeField] private float _speedIncrement;
+        private float _speed;
         private Vector2 _direction;
+        private Rigidbody2D _rigid;
 
         private void Awake()
         {
+            _speed = _initialSpeed;
             _direction = new Vector2(Random.value, Random.value).normalized;
-        }
-        private void Update()
-        {
-            gameObject.transform.Translate(_speed * Time.deltaTime * _direction);
+            _rigid = GetComponent<Rigidbody2D>();
+            _rigid.velocity = _direction * _speed;
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
+            _speed = Mathf.Min(_maximalSpeed, _speed+_speedIncrement);
             _direction = Vector2.Reflect(_direction, collision.transform.up);
-            _speed += 0.1f;
+            _rigid.velocity = _direction * _speed;
         }
     }
 }
