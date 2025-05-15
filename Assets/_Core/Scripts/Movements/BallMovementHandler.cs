8024f00645ba6f370bfffc0f703a238879314255
MagnaAxel <axel.magna.pro@gmail.com>
2025-05-15 16:20:52 +0200

    Start ball with a unit vector to right or left

--- Assets/_Core/Scripts/Movements/BallMovementHandler.cs
+++ Assets/_Core/Scripts/Movements/BallMovementHandler.cs
@@ -6,8 +6,8 @@ namespace Pong.Movements
     {
         private void Start()
         {
-            Vector2 direction = new(Random.value < 0.5f ? -1 : 1, Random.Range(-1f, 1f));
-            SetMovementDirection(direction.normalized);
+            Vector2 vector2 = Random.value < 0.5f ? Vector2.left : Vector2.right;
+            SetMovementDirection(vector2);
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -19,6 +19,7 @@ namespace Pong.Movements
                 direction -= (Vector2)collision.transform.position - collision.contacts[0].point;
             }
 
+            Debug.Log(direction.normalized);
             SetMovementDirection(direction.normalized);
         }
     }
