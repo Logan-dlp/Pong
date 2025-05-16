ab3357070050f2fcd65f4817118d005305a77543
Logan <pix.59700@gmail.com>
2025-05-16 11:36:39 +0200

    Remove Log & change variable name

--- Assets/_Core/Scripts/Movements/MovementHandlers/BallMovementHandler.cs
+++ Assets/_Core/Scripts/Movements/MovementHandlers/BallMovementHandler.cs
@@ -6,20 +6,18 @@ namespace Pong.Movements.MovementHandlers
     {
         private void Start()
         {
-            Vector2 vector2 = Random.value < 0.5f ? Vector2.left : Vector2.right;
-            SetMovementDirection(vector2);
+            Vector2 direction = Random.value < 0.5f ? Vector2.left : Vector2.right;
+            SetMovementDirection(direction);
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             Vector2 direction = Vector2.Reflect(_currentDirection, collision.contacts[0].normal);
-
             if (collision.transform.TryGetComponent(out MovementHandler movementHandler))
             {
                 direction -= (Vector2)collision.transform.position - collision.contacts[0].point;
             }
-
-            Debug.Log(direction.normalized);
+            
             SetMovementDirection(direction.normalized);
         }
     }
